75933d7e3bddf36744a946e8117e716a8b9414f4
Rohan Kanade <r0h4n@users.noreply.github.com>
2017-01-24 10:44:21 +0530

    Update etcd_utils.py

--- tendrl/gluster_integration/central_store/etcd_utils.py
+++ tendrl/gluster_integration/central_store/etcd_utils.py
@@ -1,16 +1,1 @@
-from tendrl.commons.etcdobj import fields
 
-
-def to_etcdobj(cls_etcd, obj):
-    for attr, value in vars(obj).iteritems():
-        setattr(cls_etcd, attr, to_etcd_field(attr, value))
-    return cls_etcd
-
-
-def to_etcd_field(name, value):
-    type_to_etcd_fields_map = {
-        dict: fields.DictField,
-        str: fields.StrField,
-        int: fields.IntField
-    }
-    return type_to_etcd_fields_map[type(value)](name)
