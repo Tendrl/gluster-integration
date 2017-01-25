097a72ae87288727b284ebad5f7b072a091392a1
Timothy Asir J <tjeyasin@redhat.com>
2017-01-25 17:44:38 +0530

    Fix package version and tendrl-commons dependency issue
    Currently we have to update the version in many places.
    This patch provides an easy way to update the package version by
    using the following machanism.
    
    Updated Makefile to pickup package version from __init__.py file
    and add a section called "gitversion" in Makefile to update
    version in spec file based on github.
    Updated setup.py file to pickup version from version.py file
    
    tendrl-bug-id: Tendrl/gluster-integration#108
    tendrl-bug-id: Tendrl/gluster-integration#109
    Signed-off-by: Timothy Asir J <tjeyasin@redhat.com>

--- Makefile
+++ Makefile
@@ -1,13 +1,21 @@
 NAME=tendrl-gluster-integration
-VERSION=0.0.1
+VERSION := $(shell PYTHONPATH=. python -c \
+             'import version; print version.__version__' \
+             | sed 's/\.dev[0-9]*//')
 RELEASE=1
+COMMIT := $(shell git rev-parse HEAD)
+SHORTCOMMIT := $(shell echo $(COMMIT) | cut -c1-7)
+GIT_RELEASE := $(shell git describe --tags --match 'v*' \
+                 | sed 's/^v//' \
+                 | sed 's/^[^-]*-//' \
+                 | sed 's/-.*//')
 
 all: srpm
 
 clean:
 	rm -rf dist/
 	rm -rf $(NAME)-$(VERSION).tar.gz
-	rm -rf $(NAME)-$(VERSION)-1.el7.src.rpm
+	rm -rf $(NAME)-$(VERSION)-$(RELEASE).el7.src.rpm
 
 dist:
 	python setup.py sdist \
@@ -19,4 +27,18 @@ srpm: dist
 rpm: dist
 	mock -r epel-7-x86_64 rebuild $(NAME)-$(VERSION)-$(RELEASE).el7.src.rpm --resultdir=. --define "dist .el7"
 
-.PHONY: dist rpm srpm
+gitversion:
+	# Set version and release to the latest values from Git
+	$(eval VERSION := $(VERSION).dev$(GIT_RELEASE))
+	$(eval RELEASE := $(GIT_RELEASE).$(SHORTCOMMIT))
+	sed -i version.py \
+	  -e "s/^__version__ = .*/__version__ = '$(VERSION)'/"
+	sed -i tendrl-gluster-integration.spec \
+	  -e "s/^Version: .*/Version: $(VERSION)/"
+	sed -i tendrl-gluster-integration.spec \
+	  -e "s/^Release: .*/Release: $(RELEASE)/"
+
+snapshot: gitversion srpm
+
+
+.PHONY: dist rpm srpm gitversion snapshot
--- setup.py
+++ setup.py
@@ -1,16 +1,107 @@
+import re
+from setuptools import Command
 from setuptools import find_packages
 from setuptools import setup
+import subprocess
+try:
+    # Python 2 backwards compat
+    from __builtin__ import raw_input as input
+except ImportError:
+    pass
 
 
 def extract_requirements(filename):
     with open(filename, 'r') as requirements_file:
-        return [x[:-1] for x in requirements_file.readlines()]
+        return [
+            x[:-1] for x in requirements_file.readlines()
+            if not x.startswith("#") and x[:-1] != ''
+        ]
 
 install_requires = extract_requirements('requirements.txt')
 
+
+def read_module_contents():
+    with open('version.py') as app_init:
+        return app_init.read()
+
+
+def read_spec_contents():
+    with open('tendrl-gluster-integration.spec') as spec:
+        return spec.read()
+
+module_file = read_module_contents()
+metadata = dict(re.findall("__([a-z]+)__\s*=\s*'([^']+)'", module_file))
+version = metadata['version']
+
+
+class BumpVersionCommand(Command):
+    """Bump the __version__ number and commit all changes."""
+
+    user_options = [('version=', 'v', 'version number to use')]
+
+    def initialize_options(self):
+        new_version = metadata['version'].split('.')
+        new_version[-1] = str(int(new_version[-1]) + 1)  # Bump the final part
+        self.version = ".".join(new_version)
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+
+        print('old version: %s  new version: %s' %
+              (metadata['version'], self.version))
+        try:
+            input('Press enter to confirm, or ctrl-c to exit >')
+        except KeyboardInterrupt:
+            raise SystemExit("\nNot proceeding")
+
+        old = "__version__ = '%s'" % metadata['version']
+        new = "__version__ = '%s'" % self.version
+        module_file = read_module_contents()
+        with open('version.py', 'w') as fileh:
+            fileh.write(module_file.replace(old, new))
+
+        old = 'Version: %s' % metadata['version']
+        new = 'Version: %s' % self.version
+        spec_file = read_spec_contents()
+        with open('tendrl-gluster-integration.spec', 'w') as fileh:
+            fileh.write(spec_file.replace(old, new))
+
+        # Commit everything with a standard commit message
+        cmd = ['git', 'commit', '-a', '-m', 'version %s' % self.version]
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
+
+
+class ReleaseCommand(Command):
+    """Tag and push a new release."""
+
+    user_options = [('sign', 's', 'GPG-sign the Git tag and release files')]
+
+    def initialize_options(self):
+        self.sign = False
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        # Create Git tag
+        tag_name = 'v%s' % version
+        cmd = ['git', 'tag', '-a', tag_name, '-m', 'version %s' % version]
+        if self.sign:
+            cmd.append('-s')
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
+
+        # Push Git tag to origin remote
+        cmd = ['git', 'push', 'origin', tag_name]
+        print(' '.join(cmd))
+        subprocess.check_call(cmd)
+
 setup(
     name="tendrl-gluster-integration",
-    version="1.2",
+    version=version,
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*",
                                     "tests"]),
     namespace_packages=['tendrl'],
@@ -25,5 +116,6 @@ setup(
             'tendrl-gluster-integration = '
             'tendrl.gluster_integration.manager:main'
         ]
-    }
+    },
+    cmdclass={'bumpversion': BumpVersionCommand, 'release': ReleaseCommand},
 )
