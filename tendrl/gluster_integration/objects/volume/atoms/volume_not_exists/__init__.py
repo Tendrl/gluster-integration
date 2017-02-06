a0db0cb6ba0e611c68a9db9b81f43e7a238e2bb0
Shubhendu <shubhendu.tripathi@gmail.com>
2017-02-06 21:31:22 +0530

    Update __init__.py

--- tendrl/gluster_integration/objects/volume/atoms/volume_not_exists/__init__.py
+++ tendrl/gluster_integration/objects/volume/atoms/volume_not_exists/__init__.py
@@ -5,7 +5,7 @@ from tendrl.gluster_integration.objects.volume import Volume
 class VolumeNotExists(objects.GlusterIntegrationBaseAtom):
     obj = Volume
     def __init__(self, *args, **kwargs):
-        super(VolumeNotexists, self).__init__(*args, **kwargs)
+        super(VolumeNotExists, self).__init__(*args, **kwargs)
 
     def run(self):
         return True
