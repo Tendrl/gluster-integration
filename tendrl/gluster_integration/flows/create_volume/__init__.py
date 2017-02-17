b319c4ffc0251a24389d757c5298da252d725cb3
Shubhendu <shtripat@redhat.com>
2017-02-18 00:10:11 +0530

    Added in flight operation job update messages
    tendrl-bug-id: Tendrl/gluster-integration#140
    
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/gluster_integration/flows/create_volume/__init__.py
+++ tendrl/gluster_integration/flows/create_volume/__init__.py
@@ -1,7 +1,22 @@
+from tendrl.commons.event import Event
+from tendrl.commons.message import Message
 from tendrl.gluster_integration import flows
 
 
 class CreateVolume(flows.GlusterIntegrationBaseFlow):
     def run(self):
-        super(CreateVolume, self).run()
+        Event(
+            Message(
+                priority="info",
+                publisher=tendrl_ns.publisher_id,
+                payload={
+                    "message": "Starting creation flow for volume %s" %
+                    self.parameters['Volume.volname']
+                },
+                request_id=self.request_id,
+                flow_id=self.uuid,
+                cluster_id=tendrl_ns.tendrl_context.integration_id,
+            )
+        )
 
+        super(CreateVolume, self).run()
--- tendrl/gluster_integration/objects/volume/flows/start_volume/__init__.py
+++ tendrl/gluster_integration/objects/volume/flows/start_volume/__init__.py
@@ -1,3 +1,5 @@
+from tendrl.commons.event import Event
+from tendrl.commons.message import Message
 from tendrl.gluster_integration import flows
 from tendrl.gluster_integration.objects.volume import Volume
 
@@ -8,5 +10,18 @@ class StartVolume(flows.GlusterIntegrationBaseFlow):
         super(StartVolume, self).__init__(*args, **kwargs)
 
     def run(self):
-        super(StartVolume, self).run()
+        Event(
+            Message(
+                priority="info",
+                publisher=tendrl_ns.publisher_id,
+                payload={
+                    "message": "Starting volume start flow for volume %s" %
+                    self.parameters['Volume.volname']
+                },
+                request_id=self.parameters["request_id"],
+                flow_id=self.parameters["flow_id"],
+                cluster_id=tendrl_ns.tendrl_context.integration_id,
+            )
+        )
 
+        super(StartVolume, self).run()
