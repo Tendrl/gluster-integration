cc0c703511a5d7d274f4d8a5c5d603d8b8cd0c0b
Shubhendu <shtripat@redhat.com>
2017-02-26 23:06:17 +0530

    Added extra check while gstatus run and percent use of cluster
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/gluster_integration/objects/utilization/__init__.py
+++ tendrl/gluster_integration/objects/utilization/__init__.py
@@ -4,13 +4,14 @@ from tendrl.gluster_integration import objects
 
 class Utilization(objects.GlusterIntegrationBaseObject):
     def __init__(self, raw_capacity=None, usable_capacity=None,
-                 used_capacity=None, *args, **kwargs):
+                 used_capacity=None, pcnt_used=None, *args, **kwargs):
         super(Utilization, self).__init__(*args, **kwargs)
 
         self.value = 'clusters/%s/Utilization'
         self.raw_capacity = raw_capacity
         self.usable_capacity = usable_capacity
         self.used_capacity = used_capacity
+        self.pcnt_used = pcnt_used
         self._etcd_cls = _Utilization
 
 
