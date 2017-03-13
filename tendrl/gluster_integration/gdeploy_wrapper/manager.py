cb150e3d877ba2650bb14107aad2e426797b787d
nnDarshan <darshan.n.2024@gmail.com>
2017-03-13 15:20:24 +0530

    Add gdeploy-wrapper for performing gluster actions
    This patch adds gdeploy-wrapper which acts as an interface
    between gluster integration and python-gdeploy. Atoms from
    gluster integration will perform gluster related CRUD actions
    by invoking gdeploy-wrapper which in turn invokes python-
    gdeploy
    
    tendrl-bug-id: Tendrl/gluster-integration#167
    Signed-off-by: nnDarshan <darshan.n.2024@gmail.com>

---
+++ tendrl/gluster_integration/gdeploy_wrapper/manager.py
@@ -0,0 +1,74 @@
+import importlib
+import inspect
+import os
+import pkgutil
+import re
+
+from tendrl.commons.event import Event
+from tendrl.commons.message import Message
+
+from tendrl.gluster_integration.gdeploy_wrapper import provisioner_base
+
+
+class ProvisioningManager(object):
+    def __init__(self, provisioner):
+        self.gluster_provisioner = provisioner
+        try:
+            self.plugins = []
+            self.load_plugins()
+        except (SyntaxError, ValueError, ImportError) as ex:
+            raise ex
+        self.plugin = None
+        self.set_plugin()
+
+    def list_modules_in_package_path(self, package_path, prefix):
+        modules = []
+        path_to_walk = [(package_path, prefix)]
+        while len(path_to_walk) > 0:
+            curr_path, curr_prefix = path_to_walk.pop()
+            for importer, name, ispkg in pkgutil.walk_packages(
+                path=[curr_path]
+            ):
+                if ispkg:
+                    path_to_walk.append(
+                        (
+                            '%s/%s/' % (curr_path, name),
+                            '%s.%s' % (curr_prefix, name)
+                        )
+                    )
+                else:
+                    modules.append((name, '%s.%s' % (curr_prefix, name)))
+        return modules
+
+    def load_plugins(self):
+        try:
+            path = os.path.dirname(os.path.abspath(__file__)) + '/plugins'
+            pkg = 'tendrl.gluster_integration.gdeploy_wrapper.plugins'
+            plugins = self.list_modules_in_package_path(path, pkg)
+            for name, plugin_fqdn in plugins:
+                importlib.import_module(plugin_fqdn)
+        except (SyntaxError, ValueError, ImportError) as ex:
+            Event(
+                Message(
+                    priority="error",
+                    publisher=NS.publisher_id,
+                    payload={
+                        "message": "Failed to load the gluster provisioner "
+                        "plugins. Error %s" % ex
+                    },
+                    cluster_id=NS.tendrl_context.integration_id,
+                )
+            )
+            raise ex
+
+    def get_plugin(self):
+        return self.plugin
+
+    def set_plugin(self):
+        for plugin in provisioner_base.ProvisionerBasePlugin.plugins:
+            if re.search(self.gluster_provisioner.lower(), type(
+                    plugin).__name__.lower(), re.IGNORECASE):
+                self.plugin = plugin
+
+    def stop(self):
+        self.plugin.destroy()
