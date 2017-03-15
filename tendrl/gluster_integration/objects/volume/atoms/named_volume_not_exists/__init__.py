shtripat/gluster_bridge

Fixed an issue with pre run atoms
Signed-off-by: Shubhendu <shtripat@redhat.com>

tendrl/gluster_integration/objects/volume/atoms/named_volume_not_exists/__init__.py
@@ -1,3 +1,5 @@
+import etcd
+
 from tendrl.commons.event import Event
 from tendrl.commons.message import Message
 from tendrl.gluster_integration import objects
@@ -23,9 +25,14 @@ class NamedVolumeNotExists(objects.GlusterIntegrationBaseAtom):
                 cluster_id=tendrl_ns.tendrl_context.integration_id,
             )
         )
-        volumes = tendrl_ns.etcd_orm.client.read(
-            "clusters/%s/Volumes" % tendrl_ns.tendrl_context.integration_id
-        )
+        try:
+            volumes = tendrl_ns.etcd_orm.client.read(
+                "clusters/%s/Volumes" % tendrl_ns.tendrl_context.integration_id
+            )
+        except etcd.EtcdKeyNotFound:
+            # No volumes present yet for cluster, return true
+            return True
+
         for volume in volumes._children:
             fetched_volume = Volume(
                 vol_id=volume['key'].split('/')[-1]

Tendrl/gluster-integration

tendrl/gluster_integration/objects/volume/atoms/named_volume_not_exists/__init__.py
@@ -1,3 +1,5 @@
+import etcd
+
 from tendrl.commons.event import Event
 from tendrl.commons.message import Message
 from tendrl.commons import objects
@@ -23,9 +25,14 @@ class NamedVolumeNotExists(objects.BaseAtom):
                 cluster_id=NS.tendrl_context.integration_id,
             )
         )
-        volumes = NS.etcd_orm.client.read(
-            "clusters/%s/Volumes" % NS.tendrl_context.integration_id
-        )
+        try:
+            volumes = NS.etcd_orm.client.read(
+                "clusters/%s/Volumes" % NS.tendrl_context.integration_id
+            )
+        except etcd.EtcdKeyNotFound:
+            # No volumes present yet for cluster, return true
+            return True
+
         for volume in volumes._children:
             fetched_volume = Volume(
                 vol_id=volume['key'].split('/')[-1]