2d68bb7de0f127a47c628e1cb55416e579ea8cfa
Shubhendu <shtripat@redhat.com>
2017-03-22 18:50:59 +0530

    Corrected an issue while starting gluster-integration
    While starting and syncing the cluster details, the namespace
    variables were not initialized properly. So synced cluster data
    used to be wrong.
    
    tendrl-bug-id: Tendrl/gluster-integration#198
    Signed-off-by: Shubhendu <shtripat@redhat.com>

--- tendrl/gluster_integration/__init__.py
+++ tendrl/gluster_integration/__init__.py
@@ -12,6 +12,6 @@ from tendrl.commons import TendrlNS
 class GlusterIntegrationNS(TendrlNS):
     def __init__(self):
         self,
-        ns_name = "tendrl.gluster_integration"
+        ns_name = "gluster"
         ns_src = "tendrl.gluster_integration"
         super(GlusterIntegrationNS, self).__init__(ns_name, ns_src)
