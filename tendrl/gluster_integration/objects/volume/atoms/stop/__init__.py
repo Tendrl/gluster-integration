b358b8ac7cae93a58053b29b950f18a3a62cac8c
nnDarshan <darshan.n.2024@gmail.com>
2017-03-27 17:39:18 +0530

    Replace CLI calls by gdeploy calls.
    Currently all gluster CRUD operations are performed by
    gluster CLI calls. This has been replaced with gdeploy
    calls via the gdeploy-wrapper. This patch replaces CLI
    calls for create, delete, stop and start.
    
    tendrl-bug-id: Tendrl/gluster-integration#169
    Signed-off-by: nnDarshan <darshan.n.2024@gmail.com>

--- tendrl/gluster_integration/objects/volume/atoms/stop/__init__.py
+++ tendrl/gluster_integration/objects/volume/atoms/stop/__init__.py
@@ -12,41 +12,35 @@ class Stop(objects.BaseAtom):
         super(Stop, self).__init__(*args, **kwargs)
 
     def run(self):
-        Event(
-            Message(
-                priority="info",
-                publisher=NS.publisher_id,
-                payload={
-                    "message": "Stopping the volume %s" %
-                    self.parameters['Volume.volname']
-                },
-                job_id=self.parameters["job_id"],
-                flow_id=self.parameters["flow_id"],
-                cluster_id=NS.tendrl_context.integration_id,
+        if NS.gdeploy_plugin.stop_volume(
+                self.parameters.get('Volume.volname')
+        ):
+            Event(
+                Message(
+                    priority="info",
+                    publisher=NS.publisher_id,
+                    payload={
+                        "message": "Stopped the volume %s successfully" %
+                        self.parameters['Volume.volname']
+                    },
+                    job_id=self.parameters["job_id"],
+                    flow_id=self.parameters["flow_id"],
+                    cluster_id=NS.tendrl_context.integration_id,
+                )
             )
-        )
-
-        subprocess.call(
-            [
-                'gluster',
-                'volume',
-                'stop',
-                self.parameters.get('Volume.volname'),
-                '--mode=script'
-            ]
-        )
-        Event(
-            Message(
-                priority="info",
-                publisher=NS.publisher_id,
-                payload={
-                    "message": "Successfully stopped the volume %s" %
-                    self.parameters['Volume.volname']
-                },
-                job_id=self.parameters["job_id"],
-                flow_id=self.parameters["flow_id"],
-                cluster_id=NS.tendrl_context.integration_id,
+        else:
+            Event(
+                Message(
+                    priority="error",
+                    publisher=NS.publisher_id,
+                    payload={
+                        "message": "Failed to stop the volume %s" %
+                        self.parameters['Volume.volname']
+                    },
+                    job_id=self.parameters["job_id"],
+                    flow_id=self.parameters["flow_id"],
+                    cluster_id=NS.tendrl_context.integration_id,
+                )
             )
-        )
-
+            return False
         return True
